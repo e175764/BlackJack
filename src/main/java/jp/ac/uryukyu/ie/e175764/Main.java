6a253f6013fc6b23bb6708a508eae7ed9d25c11b
e175745 <33446226+e175745@users.noreply.github.com>
2018-01-13 20:39:31 +0900

    Add files via upload

--- src/main/java/jp/ac/uryukyu/ie/e175764/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175764/Main.java
@@ -1,16 +1,14 @@
-package jp.ac.uryukyu.ie.e175764;
+package jp.ac.uryukyu.ie.e175745;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Rule rule=new Rule();
         String[] deck;
-            deck = Card.make_cards();
-            System.out.println(Arrays.asList(deck));
+        deck = Card.make_cards();
+        System.out.println(Arrays.asList(deck));
         deck=rule.Shuffle(deck);
         rule.deal(deck);
-
-
     }
 }
 
