074e21e1f9898676aac3dbdf1a3129afa780c467
Tasuku Kubo <e175764@ie.u-ryukyu.ac.jp>
2018-01-13 16:04:05 +0900

    Second

--- src/main/java/jp/ac/uryukyu/ie/e175764/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175764/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Rule rule=new Rule();
+        String[] deck;
         while (true) {
             System.out.println("Please type in the number of Joker (0~2).");
             Scanner scan = new Scanner(System.in);
@@ -10,11 +12,15 @@ public class Main {
             if (jok<-1 || jok>2) {
                 System.out.println("The number of Joker is out of range.");
             } else {
-                String[] deck = Card.make_cards(jok);
+                deck = Card.make_cards(jok);
                 System.out.println(Arrays.asList(deck));
                 break;
             }
         }
+        deck=rule.Shuffle(deck);
+        rule.deal(deck);
+
+
     }
 }
 
--- src/main/java/jp/ac/uryukyu/ie/e175764/Rule.java
+++ src/main/java/jp/ac/uryukyu/ie/e175764/Rule.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 public class Rule {
+    Player p1 =new Player();
 
     public String[] Shuffle(String[] card) {
         List<String> cards = Arrays.asList(card);
@@ -12,15 +13,9 @@ public class Rule {
         return deck;
     }
 
-    public void deal(String[] deck,int num) {
-        for(int i=0;i<num;i++){
-            for(int j=0;j<2;j++){
-
-                String card_1 = deck[(i+1)*(j+1)].replaceAll("[^0-9]", "");//手札1枚目(数字データのみ)
-                int num_1 = Integer.parseInt(card_1);  //int型にcast
-                String card_2 = deck[52-(i+1)*(j+1)].replaceAll("[^0-9]", "");//2枚目(数字データのみ)
-                int num_2 = Integer.parseInt(card_2);  //int型にcast
-            }
+    public void deal(String[] deck) {
+        for(int i=0;i<2;i++){
+                p1.Player(deck[i],deck[52-i]);
         }
     }
 }
